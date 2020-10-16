ef6da2e2fdeb9e789cd3b7e384b466dac01d4376
Dp0407 <59214783+Dp0407@users.noreply.github.com>
2020-10-16 03:42:15 -0500

    Labwork1

---
+++ RGB.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import glob
+import os
+
+img = cv.imread(cv.samples.findFile("logo-usth.png"))
+
+
+#if img is None:
+ #   sys.exit("Could not read the image.")
+    
+cv.imshow("Display window", img)
+    
+b, g, r = cv.split(img)
+cv.imshow('imge1', r)
+#cv.waitKey(0)
+cv.imshow('imge2', g)
+#cv.waitKey(0)
+cv.imshow('imge3', b)
+#cv.waitKey(0)
+
+#cv.destroyAllWindows()
+
+image = cv.merge((r,g,b))
+
+cv.imshow('image', image)
+cv.waitKey(0)
+
+cv.destroyAllWindows()
